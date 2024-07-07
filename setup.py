e5f81883a1b00131dcd1815bb890c83b0c5da700
Park won-gyu <snupark@snu.ac.kr>
2024-07-07 16:38:23 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 setup(
     name="warmstart",
     version="0.1.0",
-    packages=find_packages(include=['warmstart', 'warmstart.*']),
+    packages=find_packages(),
     install_requirements=required,
     author="Park won-gyu",
     author_email="snupark@snu.ac.kr",
